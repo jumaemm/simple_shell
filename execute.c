3458e82724a96ba7d5ed2a962e45ff74abdd0b5f
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-11-16 21:43:46 +0000

    FORK CALL

--- execute.c
+++ execute.c
@@ -8,15 +8,31 @@
 void parser(char **tokens)
 {
 	char *command = NULL, *path_command = NULL;
+	pid_t pid;
 
 	if (tokens)
 	{
 		command = tokens[0];
 		path_command = path_handler(command);
+		if (path_command == NULL)
+		{
+			perror("Command does not exist");
+			return;
+		}
 
-		if (execve(path_command, tokens, NULL) == -1)
+		pid = fork();
+		if (pid == -1)
 		{
 			perror("Error: ");
 		}
+		if (pid == 0)
+		{
+			if (execve(path_command, tokens, NULL) == -1)
+			{
+				perror("Error: ");
+			}
+		}
+		else
+			wait(NULL);
 	}
 }
