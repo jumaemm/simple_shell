387695eef96c31d3b41d3f3489b21b5ceb8739c3
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-11-16 20:43:24 +0000

    prototype for path handler

--- main.h
+++ main.h
@@ -5,8 +5,12 @@
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
 extern char **environ;
 
 void parser(char **tokens);
 char **tokenizer(char *argv, ssize_t count);
+char *path_handler(char *command);
 #endif
