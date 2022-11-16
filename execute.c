f3ecc61615599f3aa499e47381dac9391674bac1
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-11-16 17:20:53 +0000

    Execute commands

---
+++ execute.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * parser - takes input array of tokens and executes a command
+ * @tokens: input array of tokens
+ * Return: Always 0
+ */
+void parser(char **tokens)
+{
+	char *command = NULL;
+
+	if (tokens)
+	{
+		command = tokens[0];
+
+		if (execve(command, tokens, NULL) == -1)
+		{
+			perror("Error: ");
+		}
+	}
+}
