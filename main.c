4ce2b9b9c300caba4cbcc281c95e5dd2759310fb
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-11-18 11:37:40 +0000

    Abstract out shell loop functionality

--- main.c
+++ main.c
@@ -8,30 +8,8 @@
  */
 int main(int argc, char **argv)
 {
-	char *prompt = "JShell$ ";
-	size_t n = 0;
-	ssize_t count;
-	char *src = NULL;
-	/*int i;*/
-	char **tokens;
-
 	(void)argc;
 	(void)argv;
-	while (1)
-	{
-		src = malloc(sizeof(char) * n);
-		if (src == NULL)
-			perror("Memory allocation failed");
-		printf("%s", prompt);
-		count = getline(&src, &n, stdin);
-		if (count == -1)
-		{
-			printf("Exiting...\n");
-			return (-1);
-		}
-		tokens = tokenizer(src, count);
-		parser(tokens);
-	}
-	free(src);
+	shell_loop();
 	return (0);
 }
