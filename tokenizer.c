8fc5ee59999a155c83371cd3ad51838c01bb08ae
Emmanuel Juma <jumaemmanuel.EJ@outlook.com>
2022-11-18 11:38:41 +0000

    Refactor tokenization

--- tokenizer.c
+++ tokenizer.c
@@ -3,40 +3,26 @@
 /**
  * tokenizer - splits input string into tokens
  * @argv: input string
- * @count: number of chars in argv
  * Return: array of tokens
  */
-char **tokenizer(char *argv, ssize_t count)
+char **tokenizer(char *argv)
 {
-	char *dest = NULL;
 	char **tokens;
 	char *token;
-	char *delim = " \t\n";
+	char *delim = " \t\r\a\n";
+	int count = TOKEN_BUFSIZE;
 	int i;
 
-	dest = malloc(sizeof(char) * count);
-	if (dest == NULL)
-	{
-		perror("Memory allocation failed");
-		return (NULL);
-	}
-	_strcpy(dest, argv);
+	tokens = malloc(sizeof(char *) * count);
+	if (!tokens)
+		exit(EXIT_FAILURE);
+
 	token = strtok(argv, delim);
-	while (token)
-	{
-		i++;
-		token = strtok(NULL, delim);
-	}
-	i++;
-	tokens = malloc(sizeof(char *) * i);
-	token = strtok(dest, delim);
 	for (i = 0; token; i++)
 	{
-		tokens[i] = malloc(sizeof(char) * strlen(token));
-		_strcpy(tokens[i], token);
+		tokens[i] = token;
 		token = strtok(NULL, delim);
 	}
-	free(dest);
 	tokens[i] = NULL;
 	return (tokens);
 }
